e8fff42e0e96ad733d8af62d094860da843d9162
Viktor Buiakov <viktorbuyakov@cloud.upwork.com>
2025-06-03 17:47:27 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <div class="container px-5 py-5">
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+    <button class="btn btn-danger">
+      <i class="bi bi-camera-video fs-5 me-2"></i>
+      Click me</button>
+  </div>
+  </template>
+
+<script setup>
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
